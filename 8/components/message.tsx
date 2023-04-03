ea166d9c4c566a70c5b5be4ddff16847c8ec2c45
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-03 12:34:43 +0900

    파일 및 폴더 정리

--- 8/components/message.tsx
+++ 8/components/message.tsx
@@ -1,4 +1,4 @@
-import { cls } from "../libs/utils";
+import { cls } from "../libs/client/utils";
 
 interface MessageProps {
   message: string;
@@ -16,8 +16,7 @@ export default function Message({
       className={cls(
         "flex  items-start",
         reversed ? "flex-row-reverse space-x-reverse" : "space-x-2"
-      )}
-    >
+      )}>
       <div className="w-8 h-8 rounded-full bg-slate-400" />
       <div className="w-1/2 text-sm text-gray-700 p-2 border border-gray-300 rounded-md">
         <p>{message}</p>
