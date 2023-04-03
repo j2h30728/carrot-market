204a09a5eae5471b3c786b70ceac27c4f29831b3
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-03 16:36:35 +0900

    9.1 account login: 계정이없다면 계정생성하기-upsert()사용

--- 8/pages/api/users/enter.tsx
+++ 8/pages/api/users/enter.tsx
@@ -1,9 +1,24 @@
+import client from "@/libs/server/client";
 import withHandler from "@/libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
-function handler(req: NextApiRequest, res: NextApiResponse) {
+async function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log("sever", req.body);
-  return res.status(201).json({ email: req.body.email, phone: req.body.phone });
+  const { email, phone } = req.body;
+  const payload = phone ? { phone: +phone } : { email };
+  const user = await client.user.upsert({
+    where: {
+      ...payload,
+    },
+    create: {
+      name: "Anonymous",
+      ...payload,
+    },
+    update: {},
+  });
+  console.log(user);
+
+  return res.status(201).json({ text: "로그인 되었습니다." });
 }
 
 export default withHandler("POST", handler);
