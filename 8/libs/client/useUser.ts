3e6c144c299fcee00ca9bcd2da59829bda5af094
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-04 16:07:17 +0900

    10.3 useState에서 SWR로 변경시켜서 데이터를 캐싱시키자

--- 8/libs/client/useUser.ts
+++ 8/libs/client/useUser.ts
@@ -1,18 +1,7 @@
-import { useEffect, useState } from "react";
-import { useRouter } from "next/router";
+import useSWR from "swr";
 
+const fetcher = (url: string) => fetch(url).then(res => res.json());
 export default function useUser() {
-  const [user, setUser] = useState();
-  const router = useRouter();
-  useEffect(() => {
-    fetch("/api/users/me")
-      .then(response => response.json())
-      .then(data => {
-        if (!data.ok) {
-          return router.replace("/enter");
-        }
-        setUser(data.profile);
-      });
-  }, [router]);
-  return user;
+  const { data, error } = useSWR("/api/users/me", fetcher);
+  return data;
 }
