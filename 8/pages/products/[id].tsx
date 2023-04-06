30e550cff301867ea15650a45bbaf129194a11d9
LeeJiHyeon <rachel2148072@gmail.com>
2023-04-06 16:25:36 +0900

    11.5 related products: 원하는 데이터를 가져오기위해 prisma 에서 제공하는 OR,AND연산자를 사용

--- 8/pages/products/[id].tsx
+++ 8/pages/products/[id].tsx
@@ -3,16 +3,23 @@ import Button from "@/components/button";
 import Layout from "@/components/layout";
 import { useRouter } from "next/router";
 import useSWR from "swr";
-import products from "../api/products";
 import Link from "next/link";
+import { Product, User } from "@prisma/client";
+
+interface ProductWithUser extends Product {
+  user: User;
+}
+interface ItemDetailResponse {
+  ok: boolean;
+  product: ProductWithUser;
+  relatedProducts: Product[];
+}
 
 const ItemDetail: NextPage = () => {
   const router = useRouter();
-  console.log(router.query);
-  const { data, error } = useSWR(
+  const { data, error } = useSWR<ItemDetailResponse>(
     router.query.id ? `/api/products/${router.query.id}` : null
   );
-  console.log(data, error);
   return (
     <Layout canGoBack>
       <div className="px-4  py-4">
@@ -63,11 +70,15 @@ const ItemDetail: NextPage = () => {
         <div>
           <h2 className="text-2xl font-bold text-gray-900">Similar items</h2>
           <div className=" mt-6 grid grid-cols-2 gap-4">
-            {[1, 2, 3, 4, 5, 6].map((_, i) => (
-              <div key={i}>
-                <div className="h-56 w-full mb-4 bg-slate-300" />
-                <h3 className="text-gray-700 -mb-1">Galaxy S60</h3>
-                <span className="text-sm font-medium text-gray-900">$6</span>
+            {data?.relatedProducts?.map(product => (
+              <div key={product.id}>
+                <Link href={`/products/${product.id}`}>
+                  <div className="h-56 w-full mb-4 bg-slate-300" />
+                  <h3 className="text-gray-700 -mb-1">{product.name}</h3>
+                  <span className="text-sm font-medium text-gray-900">
+                    $ {product.price}
+                  </span>
+                </Link>
               </div>
             ))}
           </div>
